2058cf20678e98bfe13dc6da79780010002de08a
Adam Haney <adam.haney@retickr.com>
2012-02-13 14:15:14 -0500

    * Added support for additional headers to json_out, json_err, etc * Cleaned up the mapping from http request type to functions of that name within objects * Extended documentation

--- sleepy/base.py
+++ sleepy/base.py
@@ -13,6 +13,7 @@ __author__ = "Adam Haney"
 __license__ = "Copyright (c) 2011 Retickr"
 
 from django.http import HttpResponse, HttpResponseRedirect
+from django.utils.encoding import iri_to_uri
 from collections import OrderedDict
 import json
 import pycassa
@@ -41,40 +42,29 @@ class Base:
         This simply handles routing for the request types and in the case
         of HEAD requests suppliments implemented GET requests
         """
-        self.response = HttpResponse(mimetype='application/json')
         self.kwargs = kwargs
         self.request = request
 
         self._pre_call_wrapper(request, *args, **kwargs)
 
-        if request.method == 'GET' and hasattr(self, 'GET'):
-            self.result = self.GET(request)
-        elif request.method == 'POST' and hasattr(self, 'POST'):
-            self.result = self.POST(request)
-        elif request.method == 'PUT' and hasattr(self, 'PUT'):
-            self.result = self.PUT(request)
-        elif request.method == 'DELETE' and hasattr(self, 'DELETE'):
-            self.result = self.DELETE(request)
+        if hasattr(self, request.method):
+            result = getattr(self, request.method)(request, *args, **kwargs)
 
-        elif (
-            request.method == 'HEAD'
-            and (
-                hasattr(self, 'GET') or hasattr(self, 'HEAD')
-                )
-            ):
-            if hasattr(self, 'HEAD'):
-                self.result = self.HEAD(request)
-            else:
-                get_result = self.GET(request)
-                for k, v in get_result:
-                    self.result[k] = get_result[k]
+        elif request.method == 'HEAD'and hasattr(self, 'GET'):
+            get_result = self.GET(request)
+            for k, v in get_result:
+                result[k] = get_result[k]
         else:
-            self.response.response_code = 405
-            self.result = self.json_err(
+            result.status_code = 405
+            result = self.json_err(
                 "Resource does not support {0} for this method".format(
                     request.method))
 
-        return self.result
+        # if supress_error_codes is set make all response codes 200
+        if "suppress_response_codes" in request.REQUEST:
+            result.status_code = 200
+
+        return result
 
     def _pre_call_wrapper(self, request, *args, **kwargs):
         pass
@@ -96,11 +86,13 @@ class Base:
         cp = copy.deepcopy(self)
         return cp._call_wrapper(request, *args, **kwargs)
 
-    def json_err(self,
-                 error,
-                 error_type="Error",
-                 error_code=400,
-                 meta_data=None):
+    def json_err(
+        self,
+        error,
+        error_type="Error",
+        error_code=400,
+        meta_data=None,
+        headers=None):
         """
         Takes a string which describes the error that occured and returns a
         django object containing a json_encoded error message
@@ -108,36 +100,69 @@ class Base:
         :Parameters:
         error : string
           a string describing the error that occured
+        error_type : string
+          the 'class' of the error 'Parameter Error', 'Authentication Error'
+          this is useful of you would like to provide a small number of
+          error types that clients might deal with in different ways.
         error_code : string
           the http response code that will be passed, default is 400
+        meta_data : dictionary
+          A dictionary of elements that will be placed on the same level
+          as the 'error' key. This is helpful if you would like to
+          provide some sort of additional information to the user about the
+          error that doesn't make sense inside of the error tag.
+          The api version would be an example of something that might
+          go here.
+        headers : dictionary
+          A dictionary of header information. The key will be the header
+          type and the value will be the value of the header.
         """
 
+        api_response = HttpResponse(mimetype='application/json')
+
+        # Set meta_data to an empty dictionary if it's None
         if None == meta_data:
             meta_data = {}
 
+        # Set headers to an empty dictionary if it's None
+        if None == headers:
+            headers = {}
+
+        # Format the error response into our common format
         response = {'error': {'message': error, 'type': error_type}}
 
         if len(meta_data) > 0:
             response.update(meta_data)
 
+        # Dump out a JSON representation of the responsde
         return_string = json.dumps(response)
+
+        # Handle JSONP
         if "callback" in self.request.REQUEST:
-            self.response = HttpResponse(mimetype='text/javascript')
+            api_response = HttpResponse(mimetype='text/javascript')
             return_string = "%s(%s)" % (self.request.REQUEST["callback"],
                                         return_string)
-        self.response.write(return_string)
 
-        if not "suppress_response_codes" in self.request.REQUEST:
-            self.response.status_code = error_code
+        # Set the value of the response
+        api_response.write(return_string)
 
-        return self.response
+        # set the response code
+        api_response.status_code = error_code
 
-    def json_out(self,
-                 data,
-                 meta_data=None,
-                 cgi_escape=True,
-                 indent=None,
-                 status_code=200):
+        # Set headers
+        for k, v in headers.items():
+            api_response[k] = v
+
+        return api_response
+
+    def json_out(
+        self,
+        data,
+        meta_data=None,
+        cgi_escape=True,
+        indent=None,
+        status_code=200,
+        headers=None):
         """
         json_out takes a python datastructure (list, dict,
         OrderedDict, etc) as an argument and returns a django response
@@ -158,9 +183,18 @@ class Base:
             The ammount of whitespace to indent for each level of json
           status_code : integer
             The HTTP response code to pass back for the response
+          headers : dictionary
+            A dictionary representing the response headers we would
+            like to send back for this request
         """
+
+        api_response = HttpResponse(mimetype='application/json')
+
         if None == meta_data:
             meta_data = {}
+
+        if None == headers:
+            headers = {}
 
         response = {'data': data}
         response.update(meta_data)
@@ -171,18 +205,23 @@ class Base:
         json_string = json.dumps(response, indent=indent)
 
         if "callback" in self.request.REQUEST:
-            self.response = HttpResponse(mimetype='text/javascript')
+            api_response = HttpResponse(mimetype='text/javascript')
             json_string = "{0}({1})".format(
                 self.request.REQUEST["callback"],
                 json_string)
 
-        self.response.write(json_string)
-        self.response.status_code = status_code
-        return self.response
+        api_response.write(json_string)
 
-    def blob_out(self, data, content_type):
+        api_response.status_code = status_code
+
+        for k, v in headers.items():
+            api_response[k] = v
+
+        return api_response
+
+    def blob_out(self, data, content_type, headers=None):
         """
-        blob_out takes a bytestring with blob conten
+        blob_out takes a bytestring with blob content
         and a content_type and returns the blob
         object as an HttpResponse
 
@@ -191,18 +230,35 @@ class Base:
             A byte string of the binary data we wish to output
           content_type : string
             A string describing the MIME type of the binary object
+          headers : dictionary
+            A dictionary representing the headers we would like to
+            use for the response
         """
-         
-        response = HttpResponse(
+
+        if None == headers:
+            headers = {}
+
+        api_response = HttpResponse(
             data,
             mimetype=content_type,
             content_type=content_type)
-        response["Pragma"] = "no-cache"
-        response["Cache-Control"] = "no-cache"
-        response["Content-Length"] = len(data)
+
+        api_response["Pragma"] = "no-cache"
+        api_response["Cache-Control"] = "no-cache"
+        api_response["Content-Length"] = len(data)
+
+        for k, v in headers.items():
+            api_response[k] = v
+
         return response
 
-    def redirect_out(self, url, meta_info=None, url_key_name="url", status_code=302):
+    def redirect_out(
+        self,
+        url,
+        meta_info=None,
+        url_key_name="url",
+        status_code=302,
+        headers=None):
         """
         Outputs an HTTP 302 redirect response to a given url with
         optional contents. This can be handy so if we want to redirect
@@ -229,6 +285,12 @@ class Base:
             NOTE: if the requst passes suppress_response_codes this
             parameter will be ignored
         """
+        if None == headers:
+            headers = {}
+
+        api_response = HttpResponse(mimetype='application/json')
+        api_response['Location'] = iri_to_uri(url)
+        api_response.status_code = status_code
 
         if not meta_info:
             meta_info = {}
@@ -236,16 +298,13 @@ class Base:
         response = {"data": {url_key_name: url}}
         response.update(meta_info)
 
-        json_string = json.dumps(response)
+        api_response.write(json.dumps(response))
 
-        if not "suppress_response_codes" in self.request.REQUEST:
-            response = HttpResponseRedirect(url)
-        else:
-            response = HttpResponse()
+        for k, v in headers.items():
+            api_response[k] = v
 
-        response.write(json_string)
+        return api_response
 
-        return response
 
 class BaseServerError(Base):
     def __format_traceback(self, traceback):
@@ -256,7 +315,6 @@ class BaseServerError(Base):
                 html_string += "<li>{0}</li>".format(elm)
             html_string += "</ul>"
         return html_string
-
 
     def _call_wrapper(self, request):
         self.response = HttpResponse(mimetype='application/json')
@@ -273,8 +331,14 @@ class BaseServerError(Base):
         trace = OrderedDict()
         trace['uri'] = [request.path]
         trace['traceback'] = tb
-        trace['get'] = {str(k): str(request.GET[k]) for k in request.GET }
-        trace['post'] = {str(k): str(request.GET[k]) for k in request.POST}
+        trace['get'] = {
+            str(k): str(request.GET[k])
+            for k in request.GET
+            }
+        trace['post'] = {
+            str(k): str(request.GET[k])
+            for k in request.POST
+            }
         trace['files'] = request.FILES.keys()
         trace['globals'] = {str(k): str(v) for k, v in globals().items()}
         trace['locals'] = {str(k): str(v) for k, v in locals().items()}
