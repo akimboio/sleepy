c043c7df6e51b2d4b313d1d3fec7a19c85b1440a
Travis Truett <travis.truett@akimbo.io>
2012-09-27 13:37:47 -0400

    * Updated versioning information

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ os.system("pip install -r requirements.txt")
 
 setup(
     name="sleepy",
-    version="0.9.23",
+    version="0.9.25",
     author="Adam Haney",
     author_email="adam.haney@retickr.com",
     description=("""A RESTful library that is used at retickr on top"""\
