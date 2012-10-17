ec6641745628a35e8d8e2e66633a4e2c17785069
Wes Okes <wokes@lamppostgroup.com>
2012-10-17 14:59:38 -0400

    * Added tests for keypath functions

--- sleepy/decorators.py
+++ sleepy/decorators.py
@@ -139,25 +139,101 @@ def ParameterTransform(param, func):
 
 
 def value_for_keypath(dict, keypath):
+    """
+    Returns the value of a keypath in a dictionary
+    if the keypath exists or None if the keypath
+    does not exist.
+    
+    >>> value_for_keypath({}, '')
+    {}
+    >>> value_for_keypath({}, 'fake')
+    
+    >>> value_for_keypath({}, 'fake.path')
+    
+    >>> value_for_keypath({'fruit': 'apple'}, '')
+    {'fruit': 'apple'}
+    >>> value_for_keypath({'fruit': 'apple'}, 'fruit')
+    'apple'
+    >>> value_for_keypath({'fruit': 'apple'}, 'fake')
+    
+    >>> value_for_keypath({'fruit': 'apple'}, 'fake.path')
+    
+    >>> value_for_keypath({'fruits': {'apple': 'red', 'banana': 'yellow'}}, '')
+    {'fruits': {'apple': 'red', 'banana': 'yellow'}}
+    >>> value_for_keypath({'fruits': {'apple': 'red', 'banana': 'yellow'}}, 'fruits')
+    {'apple': 'red', 'banana': 'yellow'}
+    >>> value_for_keypath({'fruits': {'apple': 'red', 'banana': 'yellow'}}, 'fruits.apple')
+    'red'
+    >>> value_for_keypath({'fruits': {'apple': {'color': 'red', 'taste': 'good'}}}, 'fruits.apple')
+    {'color': 'red', 'taste': 'good'}
+    >>> value_for_keypath({'fruits': {'apple': {'color': 'red', 'taste': 'good'}}}, 'fruits.apple.color')
+    'red'
+    >>> value_for_keypath({'fruits': {'apple': {'color': 'red', 'taste': 'good'}}}, 'fruits.apple.taste')
+    'good'
+    """
+    
+    if len(keypath) == 0:
+        return dict
+    
     keys = keypath.split('.')
     value = dict
     for key in keys:
         if key in value:
             value = value[key]
         else:
-            value = None
+            return None
+    
     return value
 
 def set_value_for_keypath(dict, keypath, value):
+    """
+    Sets the value for a keypath in a dictionary
+    if the keypath exists. This modifies the
+    original dictionary.
+    
+    >>> set_value_for_keypath({}, '', None)
+    
+    >>> set_value_for_keypath({}, '', 'test value')
+    
+    >>> set_value_for_keypath({'fruit': 'apple'}, '', None)
+    
+    >>> set_value_for_keypath({'fruit': 'apple'}, '', 'test value')
+    
+    >>> set_value_for_keypath({'fruit': 'apple'}, 'fruit', None)
+    {'fruit': None}
+    >>> set_value_for_keypath({'fruit': 'apple'}, 'fruit', 'test value')
+    {'fruit': 'test value'}
+    >>> set_value_for_keypath({'fruit': 'apple'}, 'fake', None)
+    
+    >>> set_value_for_keypath({'fruit': 'apple'}, 'fake', 'test value')
+    
+    >>> set_value_for_keypath({'fruit': {'apple': 'red'}}, 'fruit.apple', 'green')
+    {'fruit': {'apple': 'green'}}
+    >>> set_value_for_keypath({'fruit': {'apple': 'red'}}, 'fruit.apple', None)
+    {'fruit': {'apple': None}}
+    >>> set_value_for_keypath({'fruit': {'apple': {'color': 'red'}}}, 'fruit.apple.fake', 'green')
+    
+    >>> set_value_for_keypath({'fruit': {'apple': {'color': 'red'}}}, 'fruit.apple.color', 'green')
+    {'fruit': {'apple': {'color': 'green'}}}
+    
+    """
+    
+    if len(keypath) == 0:
+        return None
+    
     keys = keypath.split('.')
     if len(keys) > 1:
         key = keys[0]
         if key in dict:
-            set_value_for_keypath(dict[key], '.'.join(keys[1:]), value)
-        return
+            if set_value_for_keypath(dict[key], '.'.join(keys[1:]), value):
+                return dict
+        return None
     
     if keypath in dict:
         dict[keypath] = value
+        return dict
+    else:
+        return None
     
 
 def OnlyNewer(element_key, keypath="data"):
@@ -226,3 +302,7 @@ def AbsolutePermalink(func, protocol="https://"):
         return u"{0}{1}{2}".format(protocol, domain, path)
     return inner
 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
