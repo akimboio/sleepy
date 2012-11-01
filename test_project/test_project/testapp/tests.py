7338f8289d81893cc441d420c38131dd4ef0b6f5
joshmarlow <josh.marlow@retickr.com>
2012-11-01 17:20:22 +0000

    * Added numerous tests for the new Paginate decorator

--- test_project/test_project/testapp/tests.py
+++ test_project/test_project/testapp/tests.py
@@ -5,26 +5,256 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+# Universe imports
+import json
+
+# Third party imports
 from django.test import TestCase, Client
+
 
 class SimpleTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def no_get_variable_test(self):
-        response = self.client.get("/test_list")
+    def test_no_get_variable(self):
+        response = self.client.get(
+            "/simple_test_list")
         response = json.loads(response.content)
-        assert response["data"] == ["a", "b", "c"], response["data"]
 
-    def if_range_get_variable_test(self):
-        response = self.client.get("/test_list", {"_if_range": "b"})
-        response = json.loads(response.conten)
-        assert response["data"] == ["a"], response["data"]
+        expected_output = [{"id": "a"}, {"id":"b"}, {"id":"c"}]
 
-    def if_range_header_test(self):
-        response = self.client.get("/test_list", If_Range="b")
-        response = json.loads(response.conten)
-        assert response["data"] == ["a"], response["data"]
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+    def test_if_range_get_variable(self):
+        response = self.client.get(
+            "/simple_test_list",
+            {"_if_range": "b"})
+        response = json.loads(response.content)
+
+        expected_output = [{"id":"a"}]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
 
 
-        
+    def test_if_range_header(self):
+        response = self.client.get(
+            "/simple_test_list",
+            **{"If-Range":"b"})
+        response = json.loads(response.content)
+
+        expected_output = [{"id":"a"}]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+
+class PaginationTestWithDefaultArgs(TestCase):
+    """
+    Note: for the purposes of this test,
+    we assume the default offset is 0 and
+    the default num_stories is 25.
+    """
+    test_server_domain = "http://testserver"
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_older_action_with_default_args(self):
+        response = self.client.get(
+            "/complex_test_list")
+        response = json.loads(response.content)
+
+        # Verify expected output
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(25)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+        # Verify that the older link gives the next page of data
+        actions = response["actions"]
+
+        older_api_call = actions["older"]["endpoint"]
+
+        # Remove the parts that reference a domain
+        api_path = older_api_call.replace(
+            self.test_server_domain, "")
+
+        response2 = self.client.get(
+            api_path
+            )
+        response2 = json.loads(response2.content)
+
+        expected_output2 = [
+            {"id": idx}
+            for idx
+            in range(25, 50)]
+
+        assert response2["data"]["stories"] == expected_output2, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output2,
+                response2["data"]["stories"]))
+
+
+class PaginationTestWithOffset(TestCase):
+    test_server_domain = "http://testserver"
+
+    def setUp(self):
+        response = self.client.get(
+            "/complex_test_list",
+            {
+                "offset": 30
+            })
+        response = json.loads(response.content)
+
+        # Verify expected output
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(30, 55)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+        self.actions = response['actions']
+
+    def test_older_action_with_offset(self):
+        """
+        """
+        # Verify that the older link gives the next page of data
+        older_api_call = self.actions["older"]["endpoint"]
+
+        # Remove the parts that reference a domain
+        api_path = older_api_call.replace(
+            self.test_server_domain, "")
+
+        response = self.client.get(
+            api_path
+            )
+        response = json.loads(response.content)
+
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(55, 80)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+    def test_newer_action_with_offset(self):
+        # Verify that the older link gives the next page of data
+        newer_api_call = self.actions["newer"]["endpoint"]
+
+        # Remove the parts that reference a domain
+        api_path = newer_api_call.replace(
+            self.test_server_domain, "")
+
+        response = self.client.get(
+            api_path
+            )
+        response = json.loads(response.content)
+
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(5, 30)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+
+class PaginationTestWithOffsetAndIfRange(TestCase):
+    test_server_domain = "http://testserver"
+
+    def setUp(self):
+        response = self.client.get(
+            "/complex_test_list",
+            {
+                "offset": 30,
+                "_if_range": 10,
+                "_get_older": True,
+            })
+        response = json.loads(response.content)
+
+        # Verify expected output
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(40, 65)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+        self.actions = response['actions']
+
+    def test_older_action_with_offset_and_if_range(self):
+        """
+        """
+        # Verify that the older link gives the next page of data
+        older_api_call = self.actions["older"]["endpoint"]
+
+        # Remove the parts that reference a domain
+        api_path = older_api_call.replace(
+            self.test_server_domain, "")
+
+        response = self.client.get(
+            api_path
+            )
+        response = json.loads(response.content)
+
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(65, 90)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+        self.actions = response['actions']
+
+    def test_newer_action_with_offset_and_if_range(self):
+        # Verify that the older link gives the next page of data
+        older_api_call = self.actions["newer"]["endpoint"]
+
+        # Remove the parts that reference a domain
+        api_path = older_api_call.replace(
+            self.test_server_domain, "")
+
+        response = self.client.get(
+            api_path
+            )
+        response = json.loads(response.content)
+
+        expected_output = [
+            {"id": idx}
+            for idx
+            in range(5, 10)]
+
+        assert response["data"]["stories"] == expected_output, (
+            "Expected: '{0}', received: '{1}'".format(
+                expected_output,
+                response["data"]["stories"]))
+
+        self.actions = response['actions']
--- test_project/test_project/testapp/views.py
+++ test_project/test_project/testapp/views.py
@@ -1,18 +1,42 @@
 # Create your views here.
 
+# Akimbo imports
 from sleepy.base import Base
-from sleepy.decorators import OnlyNewer
+from sleepy.decorators import Paginate
 from sleepy.responses import api_out
 
-class ReturnListHandler(Base):
-    @OnlyNewer("id")
+
+class ReturnSimpleListHandler(Base):
+    @Paginate("id", keypath="data.stories")
     def GET(self, request, *args, **kwargs):
-        return api_out(
+        return api_out({
+            "stories":
             [
                 {"id": "a"},
                 {"id": "b"},
                 {"id": "c"}
                 ],
+            },
+            {
+                "actions": {
+                    "do_something": "stuff"
+                    }
+                }
+            )
+
+class ReturnComplexListHandler(Base):
+    @Paginate("id", keypath="data.stories")
+    def GET(self, request, *args, **kwargs):
+        """
+        Return a lot of stories to test pagination.
+        """
+        return api_out({
+            "stories":
+            [{"id": id_}
+                for id_
+                in range(100)
+                ],
+            },
             {
                 "actions": {
                     "do_something": "stuff"
